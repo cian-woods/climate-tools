905406b93320eefbbc1ece551c1459af3e4a84a9
Cian Woods <cianwoods@gmail.com>
2018-09-04 13:57:51 +0100

    my changes

--- schem.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as pl
-
-pl.figure(figsize=(10,5))
-pl.plot([0,1],[1,0.5])
-pl.axis('off')
-pl.show()
--- toPick1.py
+++
@@ -1,6 +0,0 @@
-import cPickle as pk
-def toPick1(data,filename):
-	f = open(filename,'a')
-	for i in data:
-		pk.dump(i,f)
-	pk.dump('End',f)
